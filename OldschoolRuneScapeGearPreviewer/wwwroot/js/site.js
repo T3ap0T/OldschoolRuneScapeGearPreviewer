3341f88110e2254e720f88d3b3bd6d0c1567bd05
WhyNotDivine <niels.linssen16@home.nl>
2020-09-22 14:30:21 +0200

    Home Page frontend. Added option to select a gear slot (only frontend)

--- OldschoolRuneScapeGearPreviewer/wwwroot/js/site.js
+++ OldschoolRuneScapeGearPreviewer/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+// Create a border around a clicked gear image
+function outlineClickedSlot(elem) {
+
+    let selectedElement = elem;
+
+    if (document.getElementsByClassName("selected-item").length > 0) {
+        // If the length of the collection is higher than 0 remove all the active classes
+        // While the list should always be at max 1 long, let it loop through it regardless just in case.
+        let activeElements = document.getElementsByClassName("selected-item");
+
+        for (let ii = 0; ii < activeElements.length; ii++) {
+            activeElements[ii].classList.remove("selected-item");
+        }
+    }
+
+
+    selectedElement.classList.add("selected-item");
+
+    return true;
+}
